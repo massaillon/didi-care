f2578e01df13e29b7a5b797576aaac905bcd40a7
Massaillon Silue <96022653+massaillon@users.noreply.github.com>
2026-05-26 23:31:33 +0000

    Add root build.gradle.kts with plugin management

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+    id("com.android.library") version "8.2.0" apply false
+    kotlin("android") version "1.9.21" apply false
+    kotlin("jvm") version "1.9.21" apply false
+    id("com.google.dagger.hilt.android") version "2.52" apply false
+    id("com.google.gms.google-services") version "4.4.0" apply false
+    id("com.google.firebase.crashlytics") version "2.9.10" apply false
+}
